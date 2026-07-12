29c37f9c3626423016dc60da002c699819b353f2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "container_registry_cache_rules" {
-  description = "All container_registry_cache_rule resources"
-  value       = azurerm_container_registry_cache_rule.container_registry_cache_rules
-}
 output "container_registry_cache_rules_container_registry_id" {
-  description = "List of container_registry_id values across all container_registry_cache_rules"
-  value       = [for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : v.container_registry_id]
+  description = "Map of container_registry_id values across all container_registry_cache_rules, keyed the same as var.container_registry_cache_rules"
+  value       = { for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : k => v.container_registry_id }
 }
 output "container_registry_cache_rules_credential_set_id" {
-  description = "List of credential_set_id values across all container_registry_cache_rules"
-  value       = [for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : v.credential_set_id]
+  description = "Map of credential_set_id values across all container_registry_cache_rules, keyed the same as var.container_registry_cache_rules"
+  value       = { for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : k => v.credential_set_id }
 }
 output "container_registry_cache_rules_name" {
-  description = "List of name values across all container_registry_cache_rules"
-  value       = [for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : v.name]
+  description = "Map of name values across all container_registry_cache_rules, keyed the same as var.container_registry_cache_rules"
+  value       = { for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : k => v.name }
 }
 output "container_registry_cache_rules_source_repo" {
-  description = "List of source_repo values across all container_registry_cache_rules"
-  value       = [for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : v.source_repo]
+  description = "Map of source_repo values across all container_registry_cache_rules, keyed the same as var.container_registry_cache_rules"
+  value       = { for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : k => v.source_repo }
 }
 output "container_registry_cache_rules_target_repo" {
-  description = "List of target_repo values across all container_registry_cache_rules"
-  value       = [for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : v.target_repo]
+  description = "Map of target_repo values across all container_registry_cache_rules, keyed the same as var.container_registry_cache_rules"
+  value       = { for k, v in azurerm_container_registry_cache_rule.container_registry_cache_rules : k => v.target_repo }
 }
 
